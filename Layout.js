886290a8173ecdf15d624ec52546c99f8c3687f6
FranciscoFeitosa0102 <francisco.feitosa.junior@gmail.com>
2025-10-17 22:03:09 -0300

    Add Layout.js component with initial imports

---
+++ Layout.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { createPageUrl } from "@/utils";
+import { base44 } from "@/api/base44Client";
+import {
+  LayoutDashboard,
+  Users,
+  Building2,
+  MessageSquare,
+  LogOut,
+  TrendingUp,
+  UserPlus,
+  BarChart3,
+  ChevronDown,
+  UserCog,
+  Repeat,
+  Loader2,
+  ShieldAlert,
+  Menu,
+} from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+i
